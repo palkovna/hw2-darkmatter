8eef5a8e0541a96e5356d485f2c363d552393b4d
palkovna <54986638+palkovna@users.noreply.github.com>
2019-10-21 23:58:07 +0300

    Update opt.py

--- opt.py
+++ opt.py
@@ -19,3 +19,60 @@ gradnorm : float
 x : 1-d array
     Финальное значение вектора, минимизирующего функцию потерь
 """
+
+import numpy as np
+from collections import namedtuple
+
+Result = namedtuple('Result', ('nfev', 'cost', 'gradnorm', 'x'))
+
+def gauss_newton(y, f, j, x0, k=1, tol=1e-4):
+    x = np.array(x0, dtype=np.float)
+    cost = []
+    i = 0
+    while True:
+        i += 1
+        jac = j(*x)
+        A = np.dot(jac.T, jac)
+        r = y - f(*x)
+        cost.append(0.5*np.dot(r, r))
+        b = np.dot(jac.T, r)
+        delta_x = np.linalg.solve(A, b)
+        x += k * delta_x
+        if np.linalg.norm(delta_x) <= tol:
+            break
+    gradnorm = np.linalg.norm(np.dot(jac.T, r))
+    cost = np.array(cost)
+    return Result(nfev=i, cost=cost, gradnorm=gradnorm, x=x)
+
+
+def lm(y, f, j, x0, lmbd0=1e-2, nu=2, tol=1e-4):
+    x = np.array(x0, dtype=np.float)
+    lmbd = lmbd0
+    cost = []
+    i = 0
+    delta_x = 1e+10
+    while True:
+        i += 2
+        jac = j(*x)
+        A0 = np.dot(jac.T, jac) + lmbd * np.identity(jac[1, :].size)
+        Anu = np.dot(jac.T, jac) + lmbd/nu * np.identity(jac[1, :].size)
+        r = y - f(*x)
+        cost.append(0.5 * np.dot(r, r))
+        b = np.dot(jac.T, r)
+        delta_x0 = np.linalg.solve(A0, b)
+        delta_xnu = np.linalg.solve(Anu, b)
+        F = lambda t: 0.5*np.linalg.norm(y - f(*(x + t)))**2
+        if F(delta_xnu) <= 0.5 * np.dot(r, r):
+            lmbd = lmbd/nu
+            delta_x = delta_xnu
+            x += delta_x
+        elif F(delta_xnu) <= F(delta_x0):
+            delta_x = delta_x0
+            x += delta_x
+        else:
+            lmbd = nu*lmbd
+        if np.linalg.norm(delta_x) <= tol:
+            break
+    gradnorm = np.linalg.norm(np.dot(jac.T, r))
+    cost = np.array(cost)
+    return Result(nfev=i, cost=cost, gradnorm=gradnorm, x=x)
